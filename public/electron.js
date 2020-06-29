842139462223c6c3a1b4c29372e20963de71dcd0
fernandogelin <fernando@fernandogelin.com>
2020-06-29 10:36:52 -0400

    fix: package lock

--- public/electron.js
+++ public/electron.js
@@ -83,7 +83,7 @@ let portAvailable
 let SKIP_SENDING_DEV = false
 
 const setUpPort = async () => {
-  p = await getPort(vendorId, activeComName)
+  p = await getPort(activeComName)
   if (p) {
     triggerPort = p
     portAvailable = true
