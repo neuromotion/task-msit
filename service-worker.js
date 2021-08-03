9c75d7f6ca9accd5b928b894bdf68be2447419f4
theonlydvr <theonlydvr@users.noreply.github.com>
2021-08-03 17:50:28 +0000

    deploy: 3bcb439a070fcea0c117ea01021ad92f21edfd52

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.40e9e1a1fa44bc34da2cf24e2a68f032.js"
+  "./precache-manifest.74172a60344a5473deaf1b4a398fe9b4.js"
 );
 
 self.addEventListener('message', (event) => {
